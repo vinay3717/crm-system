4bf0c39f14576ba7b4cda1e92944b4e10693aa51
vinay3717 <vinaygole06@gmail.com>
2026-02-28 21:05:08 +0530

    Added simple login page

--- CRM/crm/views.py
+++ CRM/crm/views.py
@@ -1,22 +1,28 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Deal, FollowUp
 from datetime import date
+from django.db.models import Count
 
+@login_required
 def dashboard(request):
     total_deals = Deal.objects.count()
     won_deals = Deal.objects.filter(stage='Won').count()
     lost_deals = Deal.objects.filter(stage='Lost').count()
+    pipeline_data = Deal.objects.values('stage').annotate(count=Count('stage'))
 
     overdue_followups = FollowUp.objects.filter(
         follow_up_date__lt=date.today(),
         completed=False
-    ).count()
+    )
 
     context = {
         'total_deals': total_deals,
         'won_deals': won_deals,
         'lost_deals': lost_deals,
+        'overdue_followups_count': overdue_followups.count(),
         'overdue_followups': overdue_followups,
+        'pipeline_data': pipeline_data,
     }
 
     return render(request, 'dashboard.html', context)
--- CRM/crm_project/urls.py
+++ CRM/crm_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # For login/logout
     path('', include('crm.urls')),
 ]
